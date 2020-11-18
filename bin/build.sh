681e2064f4d606d482d77f64692da0f889ecdc13
yinshipeng <yinshipeng@126.com>
2020-11-18 19:09:50 +0800

    test

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ $TRAVIS_BRANCH == "master" ]; then
+if [ "${TRAVIS_BRANCH}" == "master" ]; then
     yarn build
-elif [$TRAVIS_BRANCH == "test"]; then
+elif [ "${TRAVIS_BRANCH}" == "test" ]; then
     yarn build:test
 fi
